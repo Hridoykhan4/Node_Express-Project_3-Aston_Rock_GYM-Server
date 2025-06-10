24558ecf488be947b254fc522bd26ec7bdb66ac7
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-06-10 21:01:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// API Home
+app.get("/", (req, res) => {
+  res.send("Hii");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@conceptualdatabase.fo2palu.mongodb.net/?retryWrites=true&w=majority&appName=ConceptualDatabase`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function Main() {
+  try {
+    const gymScheduleCollection = client
+      .db("gym-schedule")
+      .collection("gymScheduleData");
+
+    // const userCollection = client.db("gym-schedule").collection("users");
+
+    // POST a schedule
+    app.post("/schedules", async (req, res) => {
+      const scheduleData = req.body;
+      const result = await gymScheduleCollection.insertOne(scheduleData);
+      res.send(result);
+    });
+
+    // Get allSchedule
+    app.get("/schedules", async (req, res) => {
+      const cursor = await gymScheduleCollection.find().toArray();
+      res.send(cursor);
+    });
+
+    // Delete a Schedule
+    app.delete("/schedule/:id", async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const result = await gymScheduleCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+Main().catch(console.dir);
+
+// Server Port
+app.listen(port, () => {
+  console.log(`Server is running on PORT: ${port}`);
+});
